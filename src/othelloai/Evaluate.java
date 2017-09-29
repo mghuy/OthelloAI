9794792fab7b821375a2180c7aab489f1fdb1567
Leng <mghuy@unca.edu>
2017-09-29 01:12:25 -0400

    first commit

---
+++ src/othelloai/Evaluate.java
@@ -0,0 +1,24 @@
+
+package othelloai;
+
+/**
+ *
+ * @author Leng
+ */
+public class Evaluate extends Board{
+    
+    public Evaluate(int color) {
+        super(color);
+    }
+    
+    public Evaluate(Board oldBoard) {
+        super(oldBoard);
+    }
+    
+    private Move alphaBeta() {
+        
+        return new Move();
+    }
+    
+    
+}
