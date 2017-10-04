9c2b867d5a327830a18970e2221002f895faa0c7
Leng Ghuy <mghuy@unca.edu>
2017-10-04 16:31:27 -0400

    Add files via upload

---
+++ OthelloAI.java
@@ -0,0 +1,46 @@
+
+package othelloai;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Leng
+ */
+public class OthelloAI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        
+        int me;
+        String move;
+        System.out.println("C Enter starting color: ");
+        String startingColor = keyboard.nextLine();
+        if (startingColor.equals("I B")) {
+            me = 1;
+            System.out.println("R B");
+        } else {
+            me = -1;
+            System.out.println("R W");
+        }
+        
+        Board game = new Board(me);
+        System.out.println(game.toString());
+        
+        while(!game.isOver()) {
+            if (me == 1) {
+                move = game.generateMove(me);
+            } else
+                move = game.getMove(me);
+            me *= -1;
+            System.out.println(move);
+            System.out.println(game.toString());
+        }
+        
+        System.out.println("C Game is Finished");
+    }
+    
+}
